5e81505dde92495ca003529c1ff1854e23b31632
Jean-Luc Walem <jl.walem@eurologiciel.be>
2026-06-20 19:42:59 +0200

    Add manifest to GICA Peppol

--- gica_peppol/__manifest__.py
+++ gica_peppol/__manifest__.py
@@ -1,51 +1,45 @@
 {
-"name": "GICA Peppol",
-"version": "17.0.2.0.0",
-"summary": "Peppol integration for Odoo using Cyber-Relais services",
-
-```
-"description": """
-```
-
-# GICA Peppol
+    'name': 'GICA Peppol',
+    'version': '17.0.2.0.0',
+    'summary': 'Peppol integration for Odoo using Cyber-Relais services',
+    'description': """
+GICA Peppol
+===========
 
 Peppol integration for Odoo Community.
 
 Main features:
+- Outbound Peppol sales documents
+- UBL BIS 3.0 XML generation
+- SFTP transmission through Cyber-Relais services
+- Inbound Peppol purchase documents
+- Vendor bill creation from UBL documents
+- Multi-company support
+""",
+    'author': 'Jean-Luc Walem',
+    'website': 'https://www.eurologiciel.be/odoo/',
+    'category': 'Accounting',
+    'license': 'LGPL-3',
 
-* Outbound Peppol sales documents
-* UBL BIS 3.0 XML generation
-* SFTP transmission through Cyber-Relais services
-* Inbound Peppol purchase documents
-* Vendor bill creation from UBL documents
-* Multi-company support
-  """,
+    'images': [
+        'static/description/cover.png',
+    ],
 
-  "author": "Jean-Luc Walem",
-  "website": "https://www.eurologiciel.be/odoo/",
-  "category": "Accounting",
-  "license": "LGPL-3",
+    'depends': [
+        'account',
+        'mail',
+    ],
 
-  "images": [
-  "static/description/cover.png",
-  ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/gica_peppol_sale_views.xml',
+        'views/gica_peppol_purchase_views.xml',
+        'views/gica_peppol_menu.xml',
+        'views/gica_peppol_i18n_terms.xml',
+        'views/res_company_views.xml',
+        'data/server_actions.xml',
+    ],
 
-  "depends": [
-  "account",
-  "mail",
-  ],
-
-  "data": [
-  "security/ir.model.access.csv",
-  "views/gica_peppol_sale_views.xml",
-  "views/gica_peppol_purchase_views.xml",
-  "views/gica_peppol_menu.xml",
-  "views/gica_peppol_i18n_terms.xml",
-  "views/res_company_views.xml",
-  "data/server_actions.xml",
-  ],
-
-  "installable": True,
-  "application": True,
-  }
-
+    'installable': True,
+    'application': True,
+}
